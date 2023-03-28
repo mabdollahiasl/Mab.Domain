10baf37085e0fe95fb65f5834cdb48be4a0a1393
mabdollahiasl <abdollahiasl@hotmail.com>
2023-03-28 14:18:46 +0430

    fix

--- Test/Data/TestContext.cs
+++ Test/Data/TestContext.cs
@@ -25,7 +25,6 @@ namespace Test.Data
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer(@"***REMOVED***");
             base.OnConfiguring(optionsBuilder);
         }
     }
